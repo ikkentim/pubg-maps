b1b534b9e0c175216cdd1fd860d2de70a28cc7cd
Tim Potze <timpotze@gmail.com>
2024-10-07 17:59:36 +0200

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/icon-192.png',
+          '/icon-512.png',
+          '/deston.html',
+          '/deston.png',
+          '/erangel.html',
+          '/erangel.webp',
+          '/paramo.html',
+          '/paramo.png',
+          '/taego.html',
+          '/taego.webp',
+          '/vikendi.html',
+          '/vikendi.jpg',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
